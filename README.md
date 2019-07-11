3a557fd1043a7d842e39a41c88325de26b4b5fcb
Marcelo Martins <727100@gmail.com>
2019-07-11 13:16:10 -0300

    iniciando manipulação de arquivos e pasta com python

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # python_arquivos
 Trabalhando com arquivos e diretorios
+<br>
+Com os auxilios:<br>
+https://www.youtube.com/watch?v=uS6ctTujvnY
---
+++ diretorio.py
@@ -0,0 +1,23 @@
+import os
+#exibe caminho até o diretorio atual
+print(os.getcwd())
+
+#exibe arquivos e pastas no diretorio atual
+print(os.listdir("."))
+
+#exibe conteudo de uma pasta "teste_pasta"
+print(os.listdir("teste_pasta"))
+
+#criando pasta "nova_pasta"
+os.mkdir("nova_pasta")
+
+#entra na pasta informada "nova_pasta"
+os.chdir("nova_pasta")
+print(os.getcwd())
+print(os.listdir("."))
+
+#abre(cria se não existir) o arquivo "teste.txt" em modo escrita "w" e o fecha em seguida
+open("teste.txt", "w").close()
+
+#cria subpastas "outro_teste/teste2/teste3"
+os.makedirs("outro_teste/teste2/teste3")
